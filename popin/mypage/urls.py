ec4ae012a4794f5732a0fe3f719b2d6f71a123ea
kmj123 <kkk990108@naver.com>
2025-07-01 21:16:02 +0900

    변경사항 저장

--- popin/mypage/urls.py
+++ popin/mypage/urls.py
@@ -5,4 +5,5 @@ app_name = 'mypage'
 urlpatterns = [
     path('mypage/',views.mypage,name='mypage'),
     path('pchange/',views.pchange,name='pchange'),
+    path('showMybiasModal/',views.showMybiasModal,name='showMybiasModal'),
 ]
--- popin/mypage/views.py
+++ popin/mypage/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+def showMybiasModal(request):
+    return render(request,'mypage/showMybiasModal.html')
+
 def pchange(request):
     return render(request,'mypage/pchange.html')
 
 def mypage(request):
-    return render(request,'mypage/mypage.html')
+    return render(request,'mypage/myp
+    age.html')
